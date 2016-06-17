f2b334a14d5e42bb02c4e361e7d1d57c929e0260
chang wui meng <chang wui meng>
2016-06-17 18:03:48 +0800

    develop

--- wp-content/themes/omc/templates/user/header.php
+++ wp-content/themes/omc/templates/user/header.php
@@ -4,9 +4,6 @@
  */ 
 use OMC\User\Main as Main;
 
-// Load scripts
-add_action( 'wp_enqueue_scripts', 'omc_load_backbone' );
-
 // Load body attribute
 add_filter( 'omc_body_attribute', function(){ return 'id="page-'.Main::current_user_page().'" class="view-page"'; }, 5 );
 
